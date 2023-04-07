029575e25fda3011a03be976d3863e5f8aa79064
Stas1407 <stanislaw.rajm@gmail.com>
2023-04-07 12:37:18 +0200

    Landing page is done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,9 @@ import { Inter } from 'next/font/google'
 import { cn } from '@/lib/utils'
 import Providers from '@/components/Providers'
 import Navbar from '@/components/Navbar'
+import { Toaster } from '@/components/ui/Toast'
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
@@ -16,14 +17,17 @@ export default function RootLayout({
       'bg-white text-slate-900 antialiased', inter.className
     )}>
       <body className='min-h-screen bg-slate-50 dark:bg-slate-900 antialiased'>
-        <Providers>{children}
-        
-        {/* @ts-expect-error Server Component */}
-        <Navbar />
+        <Providers>
+          {children}
+
+          <Toaster position='bottom-right' />
+
+          {/* @ts-expect-error Server Component */}
+          <Navbar />
         </Providers>
 
         {/* Allow for more height on mobile devices */}
-        <div className='h-40 md:hidden'/>
+        <div className='h-40 md:hidden' />
       </body>
     </html>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,23 +4,24 @@ import { FC } from 'react'
 import SignInButton from '@/components/SignInButton'
 import { buttonVariants } from '@/ui/Button'
 import SignOutButton from '@/components/SignOutButton'
+import ThemeToggle from '@/components/ThemeToggle'
 
 const Navbar = async () => {
     const session = await getServerSession()
 
     return (
-        <div className='fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-betwee'>
+        <div className='fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between mx-2'>
             <div className='container max-w-7xl mx-auto w-full flex justify-between items-center'>
                 <Link href='/' className={buttonVariants({ variant: 'link' })}>
                     Text Similarity 1.0
                 </Link>
 
                 <div className='md:hidden'>
-                    {/* <ThemeToggle /> */}
+                    <ThemeToggle />
                 </div>
 
                 <div className='hidden md:flex gap-4'>
-                    {/* <ThemeToggle /> */}
+                    <ThemeToggle />
                     <Link href='/documentation' className={buttonVariants({ variant: 'ghost' })}>
                         Documentation
                     </Link>
